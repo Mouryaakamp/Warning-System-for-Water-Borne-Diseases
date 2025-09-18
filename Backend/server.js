f1baae46665ca31c30010cfa4e2b42bd9232698a
Mourya p <mourya.aka.mp@gmail.com>
2025-09-18 11:29:37 +0530

    all related matirials added

--- Backend/server.js
+++ Backend/server.js
@@ -17,10 +17,6 @@ connectDB();
 
 app.use("/auth", authRoutes);       
 app.use("/taluk", talukRoutes);   
-app.use((req, res, next) => {
-  console.log("Request received:", req.method, req.url);
-  next();
-});
 
 app.use("/predict", predictionRoutes);
 
